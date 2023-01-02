30e7802b8392bf965a25f4ac59cf6bfd00ecbdd6
rafa-speres <rafa.scalon@gmail.com>
2023-01-02 20:08:12 -0300

    Update consulta-com-relacionamento.sql

--- aula4/consulta-com-relacionamento.sql
+++ aula4/consulta-com-relacionamento.sql
@@ -5,5 +5,4 @@ SELECT aluno.nome as "Nome do Aluno",
        curso.nome as "Nome do Curso"
   FROM aluno
   JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
-  JOIN curso ON curso.id = aluno_curso.curso_id
-  
+  JOIN curso ON curso.id = aluno_curso.curso_id /*para chegar na tabela de curso*/
